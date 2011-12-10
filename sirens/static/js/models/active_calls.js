9c3bc6561d3a47f4febc6c77d95cfba3e1604d05
Christopher Groskopf <staringmonkey@gmail.com>
2011-12-10 17:27:49 -0600

    Fix active call comparator. Closes #22.

--- sirens/static/js/models/active_calls.js
+++ sirens/static/js/models/active_calls.js
@@ -6,7 +6,7 @@ Sirens.collections.ActiveCalls = Backbone.Collection.extend({
     model: Sirens.models.ActiveCall,
     url: Sirens.API + "/active_call",
 
-    comparatorcollection: function(active_call) {
+    comparator: function(active_call) {
         return active_call.get("reported");
     }
 });
